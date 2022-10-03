bf473a89be92ec5ce196f2c63c62d0c4b67ed07d
Joong-Won Seo <Joong-Won.Seo@tum.de>
2022-10-03 23:10:00 +0200

    significant cleanup of server

--- server/server.py
+++ server/server.py
@@ -22,10 +22,13 @@ enet_type = 'efficientnet-b7'
 out_dim = 9
 image_size = 640
 
+# the model will inference the image num_folds * num_transforms times. This can significantly increase inference times
+num_folds, num_transforms = 2, 1 #5, 8
+
+
 # dataset
 class SIIMISICDataset(Dataset):
     def __init__(self, csv, split, mode, transform=None):
-
         self.csv = csv.reset_index(drop=True)
         self.split = split
         self.mode = mode
@@ -36,7 +39,7 @@ class SIIMISICDataset(Dataset):
 
     def __getitem__(self, index):
         row = self.csv.iloc[index]
-        
+
         image = cv2.imread(row.filepath)
         image = image[:, :, ::-1]
 
@@ -53,10 +56,28 @@ class SIIMISICDataset(Dataset):
         else:
             return torch.tensor(image).float(), torch.tensor(self.csv.iloc[index].target).long()
 
-transforms_val = A.Compose([
-    A.Resize(image_size, image_size),
-    A.Normalize()
-])
+
+class SingleImage(Dataset):
+    def __init__(self, image, transform=None):
+        self.image = image
+        self.transform = transform
+
+    def __len__(self):
+        return 1
+
+    def __getitem__(self, index):
+        image = self.image[:, :, ::-1]
+
+        if self.transform is not None:
+            res = self.transform(image=image)
+            image = res['image'].astype(np.float32)
+        else:
+            image = image.astype(np.float32)
+
+        image = image.transpose(2, 0, 1)
+
+        return torch.tensor(image).float()
+
 
 # model architecture
 class enetv2(nn.Module):
@@ -64,7 +85,8 @@ class enetv2(nn.Module):
 
         super(enetv2, self).__init__()
         self.n_meta_features = n_meta_features
-        self.enet = geffnet.create_model(enet_type.replace('-', '_'), pretrained=load_pretrained)
+        self.enet = geffnet.create_model(
+            enet_type.replace('-', '_'), pretrained=load_pretrained)
         self.dropout = nn.Dropout(0.5)
 
         in_ch = self.enet.classifier.in_features
@@ -80,24 +102,10 @@ class enetv2(nn.Module):
         x = self.myfc(self.dropout(x))
         return x
 
-
-# load models
-models = []
-for i_fold in range(5):
-    model = enetv2(enet_type, n_meta_features=0, out_dim=out_dim)
-    model = model.to(device)
-    model_file = os.path.join(model_dir, f'{kernel_type}_best_fold{i_fold}.pth')
-    state_dict = torch.load(model_file, map_location=device)
-    state_dict = {k.replace('module.', ''): state_dict[k] for k in state_dict.keys()}
-    model.load_state_dict(state_dict, strict=True)
-    model.eval()
-    models.append(model)
-len(models)
-
 # image transform
 def get_trans(img, I):
     if I >= 4:
-        img = img.transpose(2,3)
+        img = img.transpose(2, 3)
     if I % 4 == 0:
         return img
     elif I % 4 == 1:
@@ -108,39 +116,32 @@ def get_trans(img, I):
         return img.flip(2).flip(3)
 
 
-# load inference data
-# patient 1 image name
-in1 = 'hello'
-# patient 1 patient_id
-pi1 = 'IP23232'
-# patient 1 sex
-s1 = 'male'
-# patient 1 age
-a1 = 70.0
-# patient 1 anatom_site_general_challenge. where its located
-asgc1 = 'torso'
-# patient 1 image filepath
-f1 = 'image_cropped.jpg'
-with open('datasettesting.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["image_name", "patient_id", "sex", "age_approx", "anatom_site_general_challenge", "width", "height", "filepath"])
-    writer.writerow([in1,pi1, s1, a1, asgc1, 6000, 4000, f1])
+def load_models(n_fold=5):
+    models = []
+    for i_fold in range(n_fold):
+        model = enetv2(enet_type, n_meta_features=0, out_dim=out_dim)
+        model = model.to(device)
+        model_file = os.path.join(
+            model_dir, f'{kernel_type}_best_fold{i_fold}.pth')
+        state_dict = torch.load(model_file, map_location=device)
+        state_dict = {k.replace('module.', '')
+                                : state_dict[k] for k in state_dict.keys()}
+        model.load_state_dict(state_dict, strict=True)
+        model.eval()
+        models.append(model)
+
+    return models
 
 
-df_single_image = pd.read_csv('datasettesting.csv')
+def predict(models, cv2_image, n_test=8):
+    transforms_val = A.Compose([
+        A.Resize(image_size, image_size),
+        A.Normalize()
+    ])
 
-
-
-
-#n_test = 8
-n_test = 1
-
-print('running model...')
-
-def run_ensemble():
-    dataset_test = SIIMISICDataset(df_single_image, 'test', 'test', transform=transforms_val)
-    image = dataset_test[0]  
-    image = image.to(device).unsqueeze(0)  # a single image need to be added a new axis to act like batch_size = 1
+    image = SingleImage(cv2_image, transform=transforms_val)[0]
+    # a single image need to be added a new axis to act like batch_size = 1
+    image = image.to(device).unsqueeze(0)
 
     with torch.no_grad():
         probs = torch.zeros((image.shape[0], out_dim)).to(device)
@@ -149,45 +150,42 @@ def run_ensemble():
                 l = model(get_trans(image, I))
                 probs += l.softmax(1)
                 print('#', end='')
+        print('')
     probs /= len(models) * n_test
 
     prediction = probs[:, 6].item()
     return prediction
 
 
-def crop():
-    with Image.open('image.jpg') as image:
-        width, height = image.size
-        a, b = min(width, height), max(width, height)
-        padding = (b - a) / 2
-        test_image = image.crop((padding, 0, b-padding, a))
-        test_image.save('image_cropped.jpg', "JPEG")
+def crop(image):
+    h, w, _ = image.shape
+    s = min(w, h)
+    x, y = (w-s)//2, (h-s)//2
+    cropped = image[y:y+s, x:x+s]
+    cv2.imwrite('image_cropped.jpg', cropped)
+    return cropped
 
 
-def predict():
-    prediction = run_ensemble()
-    result = {
-        'prediction': 'Benign' if prediction < 0.5 else 'Malignant',
-        'confidence': prediction
-    }
-
-    return result
 
 
 app = Flask(__name__)
 
+models = load_models(num_folds)
+
 
 @app.route('/melanoma', methods=['POST'])
 def get_tasks():
-    i = request.files.get('image', '')
-    print(f'received image {i}')
-    i.save('image.jpg')
-    crop()
+    print(f'received request: {request}')
+    request.files.get('image', '').save('image.jpg')
+    image = cv2.imread('image.jpg')
+    cropped = crop(image)
 
-    result = predict()
+    result = predict(models, cropped, num_transforms)
 
-    print(result)
-    return jsonify(result)
+    print(f'result: {result}')
+    return jsonify({
+        'confidence': result
+    })
 
 
 if __name__ == '__main__':
